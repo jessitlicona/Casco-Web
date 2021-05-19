480019e60f9e0ae8115880490aebb1d7fc1687ba
JessitRod1199 <jessit.licona@ogilvy.com>
2021-05-19 10:12:38 -0600

    subiendo proyecto

---
+++ precache-manifest.926e8467bdc7dddd0ea5533f79c444f7.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b16c7dd13bfed8549de",
+    "url": "/Casco/css/app.baa7721a.css"
+  },
+  {
+    "revision": "6afde2c8d8b4415f30af185f12364085",
+    "url": "/Casco/fonts/Mirai-Bold.6afde2c8.ttf"
+  },
+  {
+    "revision": "131454e3113cbdbe98b7c39c8ce23637",
+    "url": "/Casco/fonts/Mirai-Light.131454e3.otf"
+  },
+  {
+    "revision": "c11549585121f4819cd3699844c8b6be",
+    "url": "/Casco/fonts/Mirai-Medium.c1154958.ttf"
+  },
+  {
+    "revision": "3c115eebd67fba106afbc90b17123027",
+    "url": "/Casco/img/Logo-Mockup.3c115eeb.jpg"
+  },
+  {
+    "revision": "3fb99aa4e720e80674e7f5efb7dfa83d",
+    "url": "/Casco/img/arrow-02.3fb99aa4.svg"
+  },
+  {
+    "revision": "8d8ce15aa53825f289eb66aa32faa7ca",
+    "url": "/Casco/img/cascoycasco-02.8d8ce15a.svg"
+  },
+  {
+    "revision": "b1972ee02d8349ae553d5458197b0135",
+    "url": "/Casco/img/direccion-02.b1972ee0.svg"
+  },
+  {
+    "revision": "a98167575629dbbc9c5e0c26a1e408b6",
+    "url": "/Casco/img/email-05.a9816757.svg"
+  },
+  {
+    "revision": "1bb5a1d73b96595fb322de6a3bee4dca",
+    "url": "/Casco/img/fax-04.1bb5a1d7.svg"
+  },
+  {
+    "revision": "835840e16bc9c9561841a587e7b1edcd",
+    "url": "/Casco/img/img-fondo-contacto.835840e1.jpg"
+  },
+  {
+    "revision": "5fd8b5beceb3f7be48d31160285c7bf1",
+    "url": "/Casco/img/img6.5fd8b5be.jpg"
+  },
+  {
+    "revision": "e912976dbe26045d0831bab09d2e8086",
+    "url": "/Casco/img/img7.e912976d.jpg"
+  },
+  {
+    "revision": "71951fb6ed59e808f751630eeb75077c",
+    "url": "/Casco/img/img8.71951fb6.jpg"
+  },
+  {
+    "revision": "2f0549013587e70a2bd5d3aed992b187",
+    "url": "/Casco/img/logo-inicio.2f054901.svg"
+  },
+  {
+    "revision": "19a0e2a5f351ad3764307ada2b8b4cde",
+    "url": "/Casco/img/men.19a0e2a5.jpg"
+  },
+  {
+    "revision": "b9bc64deb9371c42e76e7186995f7bf0",
+    "url": "/Casco/img/noti1.b9bc64de.png"
+  },
+  {
+    "revision": "db4b6cf9411ce4e1269a5c2804570ac7",
+    "url": "/Casco/img/noticias-titulo-01.db4b6cf9.svg"
+  },
+  {
+    "revision": "58e8a43c33d478cd095ea76ab13d3591",
+    "url": "/Casco/img/telefono-03.58e8a43c.svg"
+  },
+  {
+    "revision": "7f60b76df0f1683c5a519b111f7cfcef",
+    "url": "/Casco/index.html"
+  },
+  {
+    "revision": "561cfa217b3a441a64a1",
+    "url": "/Casco/js/about.1921f367.js"
+  },
+  {
+    "revision": "6b16c7dd13bfed8549de",
+    "url": "/Casco/js/app.920f1601.js"
+  },
+  {
+    "revision": "354214f16e381c3e431f",
+    "url": "/Casco/js/chunk-vendors.01599916.js"
+  },
+  {
+    "revision": "4d7fc77fd15a28e626b65caa72c125c7",
+    "url": "/Casco/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Casco/robots.txt"
+  }
+]);
